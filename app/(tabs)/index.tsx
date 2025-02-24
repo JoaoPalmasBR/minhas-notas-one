e38a08ab3c29e55cc781fb49ae4ff6b54daa6ad1
Joao Antonio <joaoantonios74@gmail.com>
2025-02-24 02:08:45 -0300

    home estilizada

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,39 @@
 import { Image, StyleSheet, Platform } from 'react-native';
-
+import * as FileSystem from 'expo-file-system';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect, useState } from 'react';
+import { Asset } from 'expo-asset';
+
 
 export default function HomeScreen() {
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      try {
+        let response;
+        if (Platform.OS === 'web') {
+          const asset = Asset.fromModule(require('../../assets/posts.json'));
+          await asset.downloadAsync();
+          response = await fetch(asset.uri).then((res) => res.json());
+        } else {
+          const asset = Asset.fromModule(require('../../assets/posts.json'));
+          await asset.downloadAsync();
+          const fileUri = asset.localUri || asset.uri;
+          response = await FileSystem.readAsStringAsync(fileUri);
+          response = JSON.parse(response);
+        }
+        
+        setPosts(response);
+      } catch (error) {
+        console.error('Erro ao carregar os posts:', error);
+      }
+    };
+    loadPosts();
+  }, []);
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -26,6 +54,16 @@ export default function HomeScreen() {
           Tap the Explore tab to learn more about what's included in this starter app.
         </ThemedText>
       </ThemedView>
+
+      {/* Renderize os posts aqui */}
+      {posts.map((post, index) => (
+        <ThemedView key={index} style={styles.postContainer}>
+          <ThemedText>{post.title}</ThemedText>
+          <ThemedText>{post.content}</ThemedText>
+        </ThemedView>
+      ))}
+
+      
       
     </ParallaxScrollView>
   );
@@ -47,5 +85,10 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  postContainer: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
 });
